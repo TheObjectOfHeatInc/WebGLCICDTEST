6b82ef3ca05abb2457894bc6ab58f9a4d871da95
Greg <TheObjectOfHeatInc@gmail.com>
2025-01-04 00:47:59 +0300

    Try to work cicd

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -1,14 +1,18 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChangeColor : MonoBehaviour
 {
     [SerializeField]private Image imageComponent;
-    private bool isBlue = false;
+    [SerializeField]private TextMeshProUGUI _textComponent;
+    private int _score = 0;
+    private bool _isBlue = false;
 
     void Start()
     {
         imageComponent = GetComponent<Image>();
+        
     }
 
     void Update()
@@ -28,8 +32,9 @@ public class ChangeColor : MonoBehaviour
 
     void CheckClick()
     {
-        imageComponent.color = isBlue ? Color.red : Color.blue;
-
-        isBlue = !isBlue;
+        imageComponent.color = _isBlue ? Color.red : Color.blue;
+        _score++;
+        _textComponent.text = _score.ToString();
+        _isBlue = !_isBlue;
     }
 }
